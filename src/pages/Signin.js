dcdee2b7931860ce75f1905a8ab45bb7dd1b1b77
조성민 <p33a33@gmail.com>
2020-09-23 16:30:13 +0900

    [Feature]'Client - Signup, Signin feature':sparkles:
    Signin.js와 Signup.js 기능구현입니다.
    
    [상세내용]
    Signin : 아이디, 비밀번호 input에 있는 Value가 로그인버튼을 통해 /signin URL로 전송됩니다.
    전송되는 형식은 다음과 같습니다.
    ex) { email : test@test.com, password : 1234 }
    
    Signup : Signup페이지 input에 있는 Value가 회원가입 버튼을 통해서 /signup URL로 POST 요청됩니다.
    POST 요청 전, 비밀번호와 비밀번호 확인에 입력된 Value의 일치 여부를 판단합니다. 일치하는 경우에만 서버로 전송됩니다.
    전송되는 형식은 다음과 같습니다.
    { email : test@test.com, password : 1234, fullname : 조성민, nickname : 성민 }

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,15 +1,55 @@
 import React from 'react';
+import axios from 'axios';
+import { Route, Redirect } from "react-router-dom";
 
-export default function Signin(props) {
-    return (
-        <div>
-            로그인
-            <form action="http://localhost:5000/signin" method="POST">
-                <label> 이메일 <input type="text" placeholder="이메일을 입력해주세요" /> </label>
-                <label> 비밀번호 <input type="password" placeholder="비밀번호를 입력해주세요" /> </label>
-                <input type="submit" value="로그인" />
-            </form>
-        </div>
-    )
+
+export default class Signin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: null,
+            password: null
+        }
+        this.submitHandler = this.submitHandler.bind(this)
+    }
+
+    emailChange(e) {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    passwordChange(e) {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    submitHandler() {
+        axios.post(`http://18.216.148.52:5000/signin`, this.state)
+            .then(res => {
+                console.log(res)
+                if (res.status === 200) {
+                    this.props.handleisSigninChange();
+                } else {
+                    return alert("아이디나 비밀번호를 다시 확인해주세요") // 왜 작동 안하져?
+                }
+            })
+    }
+
+    render() {
+        if (this.props.isSignin) {
+            return <Redirect to="/" />
+        } else {
+            return (
+                <div>
+                    <div>로그인</div>
+                    <div><label> 이메일 <input onChange={this.emailChange.bind(this)} type="text" placeholder="이메일을 입력해주세요" /> </label></div>
+                    <div><label> 비밀번호 <input onChange={this.passwordChange.bind(this)} type="password" placeholder="비밀번호를 입력해주세요" /> </label></div>
+                    <div><button onClick={this.submitHandler}> 로그인 </button></div>
+                </div >
+            )
+        }
+    }
 }
 

289dd2b30050d824883b2d411ac50247f20eb41d
조성민 <p33a33@gmail.com>
2020-09-23 13:28:12 +0900

    [Updated]'Client - Main.js skeleton':art:

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default function Main(props) {
     return (
-        <div> this is Main page </div>
+        <div> todo가 표시되는 공간입니다.
+            {props.todos.map(todo => (
+                <>
+                    <div>제목 : {todo.title}</div>
+                    <div>내용 : {todo.body}</div>
+                </>
+            )
+            )}
+        </div>
     )
 }

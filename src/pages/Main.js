34c1d4c496db9845125ac0931dc97b931f9fc57c
조성민 <p33a33@gmail.com>
2020-09-27 00:46:56 +0900

    [Fix, Update] "Client - resoloved PR conflict problem" :hammer:
    * PR 충돌로 인한 Signup, Signin 버전 문제를 해결했습니다
    * 코드 가독성 개선 수정 사항이 있었습니다.
    * 기능 추가 및 개선은 없습니다.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -14,68 +14,56 @@ class Main extends React.Component {
             body: "",
         }
         this.handleInputValue = this.handleInputValue.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleAddOpen = this.handleAddOpen.bind(this)
+        this.renderEachTodo = this.renderEachTodo.bind(this)
     }
 
     handleInputValue = (key) => (e) => {
         this.setState({ [key]: e.target.value });
     };
 
-    render() {
-        if (this.state.isAddOpened) {
-            return <div>
-                <Link to="/mypage"><h3>Mypage</h3></Link>
-                <div className="layout">
-                    <h2>Todo List</h2>
-                    {this.props.todos.map((todo) => {
-                        return <TodoEntry
-                            key={todo.id} todo={todo} handleInputValue={this.handleInputValue}
-                            handleFetchTodo={this.props.handleFetchTodo} handleEditedData={this.props.handleEditedData} />
-                    }
-                    )}
+    // render에 직접 적용된 함수를 메소드로 정리했습니다.
+    handleAdd = () => {
+        const { title, body } = this.state
+        axios.post("http://18.216.148.52:5000/todowrite", { title, body })
+            .then(res => this.props.todos.push(res.data))
+            .then(() => this.handleAddOpen())
+            .catch(err => { alert("에러가 발생했습니다. 다시 시도해주세요."); console.log(err) });
+    }
 
-                </div>
-                <div className="add-todo">
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            const { title, body } = this.state
-                            //this.props.todos.push({ title, body })
-                            //this.setState({ isAddOpened: !this.state.isAddOpened })
-                            return axios
-                                .post("http://localhost:5000/todowrite", { title, body })
-                                .then((res) => { this.props.todos.push(res.data); console.log(res) })
-                                .then(() => this.setState({ isAddOpened: !this.state.isAddOpened }))
-                                .catch((err) => {
-                                    alert("에러가 발생했습니다. 다시 시도해주세요.");
-                                    console.log(err);
-                                });
-                        }}
-                    >
-                        <div>
-                            <input type="title" placeholder="제목" onChange={this.handleInputValue("title")}></input>
-                        </div>
-                        <div>
-                            <textarea type="body" placeholder="내용" onChange={this.handleInputValue("body")}></textarea>
-                        </div>
+    handleAddOpen = () => {
+        this.setState({ isAddOpened: !this.state.isAddOpened })
+    }
+
+    renderEachTodo = (todos) => { // TodoEntry에 props가 많아서 가독성을 높이기 위해 map내용을 메소드로 따로 뺐습니다.
+        todos.map(todo =>
+            <TodoEntry
+                key={todo.id}
+                todo={todo}
+                handleInputValue={this.handleInputValue}
+                handleFetchTodo={handleFetchTodo}
+                handleEditedData={handleEditedData} />)
+    }
+
+    render() {
+        let { isAddOpened } = this.state
+        let { todos } = this.props
+
+        return (
+            <div>
+                <Link to="/mypage"><h3>Mypage</h3></Link>
+                <div className="layout"> <h2>Todo List</h2> {this.renderEachTodo(todos)} </div> {/* TodoEntry가 렌더되는 부분입니다*/}
+                <div className="add-todo" style={{ display: isAddOpened ? "block" : "none" }}> {/*  isAddOpened를 확인하여 렌더합니다. */}
+                    <form onSubmit={(e) => { e.preventDefault(); this.handleAdd(); }} >
+                        <div><input type="title" placeholder="제목" onChange={this.handleInputValue("title")} /></div>
+                        <div><textarea type="body" placeholder="내용" onChange={this.handleInputValue("body")} /></div>
                         <button type="submit">done</button>
                     </form>
                 </div>
+                <button onClick={this.handleAddOpen} style={{ display: isAddOpened ? "none" : "block" }}>add</button> {/*  Add가 열리면 Add 버튼을 숨깁니다. */}
             </div >
-        }
-        return <div>
-            <Link to="/mypage"><h3>Mypage</h3></Link>
-            <div className="layout">
-                <h2>Todo List</h2>
-                {console.log(this.props)}
-                {this.props.todos.map((todo) => {
-                    return <TodoEntry
-                        key={todo.id} todo={todo} handleInputValue={this.handleInputValue}
-                        handleFetchTodo={this.props.handleFetchTodo} handleEditedData={this.props.handleEditedData} />
-                }
-                )}
-            </div>
-            <button onClick={() => { this.setState({ isAddOpened: !this.state.isAddOpened }) }}>add</button>
-        </div>
+        )
     }
 }
 
--- src/pages/TodoEntry.js
+++ src/pages/TodoEntry.js
@@ -12,72 +12,73 @@ class TodoEntry extends React.Component {
             id: props.todo.id
         }
         this.handleInputValue = this.handleInputValue.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+        this.handleDelete = this.handleDelete.bind(this)
+        this.handleModify = this.handleModify.bind(this)
+        this.handleModifyOpen = this.handleModifyOpen.bind(this)
     }
-    //shouldComponentUpdate(nextState) {
-    //    if (nextState.isClear !== this.state.isClear) {
-    //        return false
-    //    }
-    //}
+
     handleInputValue = (key) => (e) => {
         this.setState({ [key]: e.target.value });
-    };
+    }
 
+    // render에 직접 적용된 함수를 메소드로 정리했습니다.
+    handleModifyOpen = () => {
+        this.setState({ isModifyOpened: !isModifyOpened })
+    }
+
+    handleClear = () => {
+        const data = { id: this.props.todo.id }
+        axios.post("http://18.216.148.52:5000/clear", data)
+            .then((res) => { this.props.handleEditedData(res.data); console.log(res) })
+            .catch((e) => console.log(e))
+    }
+
+    handleDelete = () => {
+        const data = { id: this.props.todo.id }
+        axios.post("http://18.216.148.52:5000/tododelete", data)
+            .then((res) => this.props.handleFetchTodo(res.data))
+            .then(this.handleModifyOpen)
+    }
+
+    handleModify = () => {
+        let { title, body, id } = this.state
+        let data = { title: title, body: body, id: id }
+        axios.post("http://18.216.148.52:5000/todoedit", data)
+            .then((res) => { this.props.handleEditedData(res.data); console.log(res) })
+            .catch((e) => console.log(e))
+            .then(this.handleModifyOpen)
+    }
 
     render() {
-        if (this.state.isModifyOpened) {
-            return <div className="todo-entry">
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    const data = {
-                        title: this.state.title,
-                        body: this.state.body,
-                        id: this.state.id
-                    }
-                    axios.
-                        post("http://localhost:5000/todoedit", data)
-                        .then((res) => { this.props.handleEditedData(res.data); console.log(res) })
-                        .catch((e) => console.log(e))
-                        .then(() => this.setState({ isModifyOpened: !this.state.isModifyOpened }))
-                        .then(() => {
-                            console.log(this.props);
-                        })
-                }}>
-                    <div className="todo-title">
-                        <input defaultValue={this.props.todo.title} onChange={this.handleInputValue("title")}></input >
-                        <input defaultValue={this.props.todo.body} onChange={this.handleInputValue("body")}></input><span><button type="submit" >수정</button>
-                            <button onClick={() => {
-                                const data = {
-                                    id: this.props.todo.id
-                                }
-                                axios.
-                                    post("http://localhost:5000/tododelete", data)
-                                    .then((res) => this.props.handleFetchTodo(res.data))
-                                    .then(() => this.setState({ isModifyOpened: !this.state.isModifyOpened }))
-                            }}>삭제</button></span></div>
+        let { title, body, isclear } = this.props.todo;
+        let { isModifyOpened } = this.state
+
+        return (
+            <div>
+                <form onSubmit={(e) => { e.preventDefault(); this.handleModify(); }} style={{ display: isModifyOpened ? "block" : "none" }}>
+                    <input defaultValue={title} onChange={this.handleInputValue("title")} />
+                    <input defaultValue={body} onChange={this.handleInputValue("body")} />
+                    <span>
+                        <button type="submit" >수정</button>
+                        <button onClick={this.handleDelete}>삭제</button>
+                    </span>
                 </form>
+                <div className="todo-entry" style={{ display: isModifyOpened ? "none" : "block" }}>
+                    <div className="todo-title">
+                        <h3 style={{ textDecorationLine: isclear ? 'line-through' : '' }}>{title}
+                            <span>
+                                <button onClick={this.handleModifyOpen}>수정</button>
+                                <button onClick={this.handleClear}>clear</button>
+                            </span>
+                        </h3>
+                    </div>
+                    <div>
+                        <h5 style={{ textDecorationLine: isclear ? 'line-through' : '' }} > {body}</h5>
+                    </div>
+                </div>
             </div>
-        }
-        return <div className="todo-entry">
-
-            <div className="todo-title">
-                <h3 style={{ textDecorationLine: this.props.todo.isclear ? 'line-through' : '' }}>{this.props.todo.title}<span><button onClick={() => {
-                    this.setState({ isModifyOpened: !this.state.isModifyOpened })
-                }}>수정</button><button onClick={() => {
-                    const data = { id: this.props.todo.id, }
-                    axios.
-                        post("http://localhost:5000/clear", data)
-                        .then((res) => { this.props.handleEditedData(res.data); console.log(res) })
-                        .catch((e) => console.log(e))
-
-                    //axios.
-                    //    post("http://localhost:5000/clear", data)
-                    //    .then((res) => this.props.handleFetchTodo(res.data))
-                    //    
-                }}>clear</button></span></h3></div>
-            <div><h5 style={{ textDecorationLine: this.props.todo.isclear ? 'line-through' : '' }} > {this.props.todo.body}</h5></div>
-
-        </div>
-
+        )
     }
 }
 

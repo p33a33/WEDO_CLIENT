4c2c1c129d6859a03ac9bb73cd935a702a0c4465
조성민 <p33a33@gmail.com>
2020-10-04 19:56:34 +0900

    [client] state 위치 변경에 따른 수정

--- src/pages/TodoEntry.js
+++ src/pages/TodoEntry.js
@@ -12,7 +12,10 @@ class TodoEntry extends React.Component {
             title: props.todo.title,
             body: props.todo.body,
             id: props.todo.id,
-            isclear: false
+            isclear: false,
+            isShareOpen: false,
+            shareTo: [],
+            friendToSearch: '',
         }
         this.handleInputValue = this.handleInputValue.bind(this)
         this.handleClear = this.handleClear.bind(this)
@@ -20,12 +23,49 @@ class TodoEntry extends React.Component {
         this.handleModify = this.handleModify.bind(this)
         this.handleModifyOpen = this.handleModifyOpen.bind(this)
         this.handleClickTitle = this.handleClickTitle.bind(this)
+        this.handleIsShareOpen = this.handleIsShareOpen.bind(this)
+        this.handleAddShareTo = this.handleAddShareTo.bind(this)
+    }
+
+    componentDidMount() {
+        console.log(this.props.todo.users)
+        let sharingList = this.props.todo.users.map(users => [users.id, users.full_name])
+
+        this.setState({ shareTo: sharingList })
+    }
+    handleIsShareOpen = () => {
+        this.setState({ isShareOpen: !this.state.isShareOpen, friendToSearch: '', })
     }
     handleInputValue = (key) => (e) => {
         this.setState({ [key]: e.target.value });
     }
     handleClickTitle = () => {
         this.setState({ isTitleClicked: !this.state.isTitleClicked })
+    }
+    handleAddShareTo = (e) => {
+        let { friendToSearch, shareTo } = this.state
+        let { followinfo } = this.props;
+        let temp = shareTo
+        let isFound = false;
+
+        for (let follower of shareTo) {
+            if (follower[1] === friendToSearch) {
+                return alert("이미 목록에 있는 친구입니다")
+            }
+        }
+
+        for (let friend of followinfo) {
+            if (friend.full_name === friendToSearch) {
+                temp.push([friend.id, friend.full_name])
+                isFound = true
+            }
+        }
+
+        if (isFound) {
+            return;
+        } else {
+            return alert("이런 친구가 있었던가요?")
+        }
     }
     // render에 직접 적용된 함수를 메소드로 정리했습니다.
     handleModifyOpen = () => {
@@ -62,20 +102,41 @@ class TodoEntry extends React.Component {
         }
     }
     render() {
-        let { title, body, isclear, id } = this.props.todo;
-        let { isModifyOpened, isTitleClicked } = this.state
+        let { title, body, isclear, users } = this.props.todo;
+        let { publicOnly } = this.props
+        let { isModifyOpened, isTitleClicked, isShareOpen, shareTo } = this.state
         return (
             <div>
-                <form className="addForm" onSubmit={(e) => { e.preventDefault(); this.handleModify(); }} style={{ display: isModifyOpened ? "block" : "none" }}>
-                    <input style={{ marginTop: '20px' }} defaultValue={title} onChange={this.handleInputValue("title")} />
-                    <textarea defaultValue={body} onChange={this.handleInputValue("body")} />
-                    <span className="editFormButtons">
-                        <button type="button" className="deleteButton" onClick={this.handleDelete}></button>
-                        <button type="submit" className="editOkay"></button>
-                        <button type="button" className="cancelButton" onClick={this.handleModifyOpen}></button>
-                    </span>
-                </form>
-                <ul className="todo-entry" style={{ display: isModifyOpened ? "none" : "block" }}>
+                <div style={{ overflow: "hidden" }}>
+                    <form className={isShareOpen ? "addForm toLeft" : "addForm"} onSubmit={(e) => { e.preventDefault(); this.handleModify(); }} style={{ display: isModifyOpened ? "block" : "none" }}>
+                        <input style={{ marginTop: '20px' }} defaultValue={title} onChange={this.handleInputValue("title")} />
+                        <textarea defaultValue={body} onChange={this.handleInputValue("body")} />
+                        <span className="editFormButtons">
+                            <button type="button" className="deleteButton" onClick={this.handleDelete}></button>
+                            <button type="submit" className="editOkay"></button>
+                            <button type="button" className="cancelButton" onClick={this.handleModifyOpen}></button>
+                            <button type="button" className="shareButton" onClick={this.handleIsShareOpen}></button>
+                        </span>
+                    </form>
+                    <div className="addForm editEntry" id="shareForm-main" style={{ display: isShareOpen ? "" : "none" }}>
+                        <input className="searchInput" placeholder="친구이름입력" onChange={this.handleInputValue("friendToSearch")}></input>  {/*친구 이름 검색창*/}
+                        <button onClick={this.handleAddShareTo} />
+                        <div id="shareTo">
+                            {shareTo.map(friend =>
+                                <div key={friend[0]}>
+                                    <div className="searchFriendEntry" >
+                                        {friend[1]}
+                                        <button userid={friend[0]} id="removeToShare" onClick={(e) => this.handleRemoveShareTo(e)} />
+                                    </div>
+                                </div>)}
+                        </div>
+                    </div>
+                </div>
+                <ul className="todo-entry" style={{
+                    display: isModifyOpened || publicOnly ?
+                        publicOnly && !isModifyOpened ?
+                            users.length > 0 ? "block" : "none" : "none" : "block"
+                }}>
                     <div className="todo-title">
                         <span className="editFormButtons">
                             <button id={isclear ? "done" : "yet"} onClick={() => { this.handleClear() }}></button>
